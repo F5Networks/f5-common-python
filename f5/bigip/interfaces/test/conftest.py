50f04e2d451b6b31a014e5320c91c1975050ef79
jlongstaf <j.longstaff@f5.com>
2015-11-24 14:56:51 -0700

    Fixes: #14 Create unit tests for interfaces
    Problem: Need to create a mock facility to enable
    unit testing interface objects.
    
    Analysis: Create Big IP mock object and sample tests for pool interface.
    
    Unit/Robot tests: py.test, flake8

--- f5/bigip/interfaces/test/conftest.py
+++ f5/bigip/interfaces/test/conftest.py
@@ -1,2 +1,6 @@
-from pytestutils.mock_import import return_mock_imports
+"""
+Commented out to fix flake8 violation. See Za for what needs to be done to use
+mock_imports.
 
+from pytestutils.mock_import import return_mock_imports
+"""
--- f5/bigip/interfaces/test/test_pool.py
+++ f5/bigip/interfaces/test/test_pool.py
@@ -2,48 +2,50 @@ from f5.bigip import exceptions
 from f5.bigip.interfaces.pool import Pool
 from f5.bigip.interfaces.test.big_ip_mock import BigIPMock
 
-import mock
 import pytest
 
+"""Note: Because of file names pased to read_json_file(), tests assume
+they are being called from f5-common-python directory. Change file paths
+if you want to call from a different directory.
 
-# @pytest.mark.usefixtures("return_mock_imports")
-class TestPool(object):
-    # import_names_to_mock = ['f5.bigip']
+Usage example:
 
-    def _create_mock_response(self, status_code, json):
-        response = mock.Mock()
-        response.status_code = status_code
-        response.text = json
+    cd <path>/f5-common-python
+    py.test f5/bigip/interfaces/test/test_pool.py
+
+    with coverage:
+    py.test --cov f5
+"""
 
 
-        return response
+def test_get_description():
+    response = BigIPMock.create_mock_response(
+        200, BigIPMock.read_json_file("f5/bigip/interfaces/test/pool.json"))
 
-    def test_get_description(self):
+    big_ip = BigIPMock(response)
+    test_pool = Pool(big_ip)
 
-        response = self._create_mock_response(200,
-                                              '{"description": "A mock pool"}')
-        big_ip = BigIPMock(response)
-        test_pool = Pool(big_ip)
+    description = test_pool.get_description("my-Pool")
+    assert description == "sdfds"
 
-        description = test_pool.get_description("pool_name")
 
-        assert description == "A mock pool"
+def test_get_description_error():
+    response = BigIPMock.create_mock_response(
+        500, BigIPMock.read_json_file("f5/bigip/interfaces/test/pool.json"))
 
-    def test_get_description_error(self):
+    big_ip = BigIPMock(response)
+    test_pool = Pool(big_ip)
 
-        response = self._create_mock_response(500, "")
-        big_ip = BigIPMock(response)
-        test_pool = Pool(big_ip)
+    # should raise a PoolQueryException
+    with pytest.raises(exceptions.PoolQueryException):
+        test_pool.get_description("my-Pool")
 
-        with pytest.raises(exceptions.PoolQueryException):
-            test_pool.get_description("pool_name")
+def test_get_load_balancing():
+    response = BigIPMock.create_mock_response(
+        200, BigIPMock.read_json_file("f5/bigip/interfaces/test/pool.json"))
 
-    def test_get_description_without_name(self):
+    big_ip = BigIPMock(response)
+    test_pool = Pool(big_ip)
 
-        response = self._create_mock_response(500, "")
-        big_ip = BigIPMock(response)
-        test_pool = Pool(big_ip)
-
-        description = test_pool.get_description()
-
-        assert description is None
+    mode = test_pool.get_lb_method("my-Pool")
+    assert mode == "ROUND_ROBIN"
