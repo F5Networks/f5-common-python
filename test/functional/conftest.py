db862ec8c6ff0ccf7f561880a2793525169ae940
Shawn Wormke <shawn@f5.com>
2016-01-07 19:54:05 -0700

    Updated copyrights to include 2016

--- test/functional/conftest.py
+++ test/functional/conftest.py
@@ -1,4 +1,4 @@
-# Copyright 2015 F5 Networks Inc.
+# Copyright 2015-2016 F5 Networks Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
