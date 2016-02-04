808f065aef703b2649dd073fe2a678e48a6ce4a7
Paul Breaux <p.breaux@f5.com>
2016-02-04 14:42:09 -0700

    Refactor iapp module into application
    Issues: Issue #76
    
    Problem: The iapp module needs to be refactored
    
    Analysis: Implemented the application module which uses the new
    OrganizingCollection, Collection, and Resource base classes to structure
    BigIP resources according to the uri structure. The application module
    contains a Template and Service resource as well as their respective
    collections. It also contains an overarching Application
    OrganizingCollection.
    
    Tests: Some sys unit tests are failing. Will rebase new changes on top
    of this to see if that fixes it.

---
+++ f5/bigip/sys/application.py
@@ -0,0 +1,206 @@
+"""Manage application services on BIG-IP using REST interface """
+# Copyright 2014-2016 F5 Networks Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from f5.bigip.resource import Collection
+from f5.bigip.resource import KindTypeMismatch
+from f5.bigip.resource import MissingRequiredCreationParameter
+from f5.bigip.resource import MissingRequiredReadParameter
+from f5.bigip.resource import OrganizingCollection
+from f5.bigip.resource import Resource
+
+from requests import HTTPError
+
+
+class Application(OrganizingCollection):
+    def __init__(self, sys):
+        super(Application, self).__init__(sys)
+        self._meta_data['allowed_lazy_attributes'] = [
+            APLScriptCollection,
+            CustomStatCollection,
+            ServiceCollection,
+            TemplateCollection
+        ]
+
+
+class APLScriptCollection(Collection):
+    def __init__(self, application):
+        super(APLScriptCollection, self).__init__(application)
+        self._meta_data['allowed_lazy_attributes'] = [APLScript]
+        self._meta_data['collection_registry'] =\
+            {'tm:sys:application:apl-script:apl-scriptstate': APLScript}
+
+
+class APLScript(Resource):
+    def __init__(self, apl_script_collection):
+        super(APLScript, self).__init__(apl_script_collection)
+        self._meta_data['required_json_kind'] =\
+            'tm:sys:application:apl-script:apl-scriptstate'
+
+
+class CustomStatCollection(Collection):
+    def __init__(self, application):
+        super(CustomStatCollection, self).__init__(application)
+        self._meta_data['allowed_lazy_attributes'] = [CustomStat]
+        self._meta_data['collection_registry'] =\
+            {'tm:sys:application:custom-stat:custom-statstate': CustomStat}
+
+
+class CustomStat(Resource):
+    def __init__(self, custom_stat_collection):
+        super(CustomStat, self).__init__(custom_stat_collection)
+        self._meta_data['required_json_kind'] =\
+            'tm:sys:application:custom-stat:custom-statstate'
+
+
+class ServiceCollection(Collection):
+    def __init__(self, application):
+        super(ServiceCollection, self).__init__(application)
+        self._meta_data['allowed_lazy_attributes'] = [Service]
+        self._meta_data['collection_registry'] =\
+            {'tm:sys:application:service:servicestate': Service}
+
+
+class Service(Resource):
+    def __init__(self, service_collection):
+        super(Service, self).__init__(service_collection)
+        self._meta_data['allowed_lazy_attributes'] = []
+	self._meta_data['required_creation_parameters'].update(('template',))
+        self._meta_data['required_refresh_parameters'].update(('partition',))
+        self._meta_data['required_json_kind'] =\
+            'tm:sys:application:service:servicestate'
+
+    def _create(self, **kwargs):
+        '''Create service on device and create accompanying Python object.
+
+        :params kwargs: keyword arguments passed in from create call
+        :raises: MissingRequiredCreationParameter
+        :raises: KindTypeMismatch
+        :raises: HTTPError
+        :returns: Python Service object
+        '''
+
+        key_set = set(kwargs.keys())
+        required_minus_received =\
+            self._meta_data['required_creation_parameters'] - key_set
+        if required_minus_received != set():
+            error_message = 'Missing required params: %r'\
+                % required_minus_received
+            raise MissingRequiredCreationParameter(error_message)
+
+        # Make convenience variable with short names for this method.
+        _create_uri = self._meta_data['container']._meta_data['uri']
+        session = self._meta_data['bigip']._meta_data['icr_session']
+
+        # Invoke the REST operation on the device.
+        try:
+            response = session.post(_create_uri, json=kwargs)
+        except HTTPError as ex:
+            # This call always returns a 404 with the following message
+            if "The configuration was updated successfully but could not be " \
+                    "retrieved" not in ex.response.text:
+                raise
+
+        # If no partition given on create, use Common
+        if 'partition' not in kwargs.keys():
+            kwargs['partition'] = 'Common'
+        # Popping out template because load was yelling at me with unexpected
+        # keyword argument
+        kwargs.pop('template', '')
+
+        # If response was created successfully, do a local_update.
+        # If not, call to overridden _load method via load
+        try:
+            if response:
+                self._local_update(response.json())
+        except NameError as ex:
+            self.load(**kwargs)
+
+        if self.kind != self._meta_data['required_json_kind']:
+            error_message = "For instances of type '%r' the corresponding" +\
+                " kind must be '%r' but creation returned JSON with kind: %r"\
+                % (self.__class__.__name__,
+                   self._meta_data['required_json_kind'],
+                   self.kind)
+            raise KindTypeMismatch(error_message)
+
+        # Update the object to have the correct functional uri.
+        self._build_meta_data_uri(self.selfLink)
+        return self
+
+    def build_uri(self):
+        '''Build a uri to access service object on BigIP'''
+        base_uri = self._meta_data['container']._meta_data['uri']
+        return '%s~%s~%s.app~%s' % (
+            base_uri,
+            self.partition,
+            self.name,
+            self.name
+        )
+
+    def update(self, **kwargs):
+        '''Push local updates to the object on the device.
+
+        :params kwargs: keyword arguments for accessing/modifying the object
+        :returns: updated Python object
+        '''
+
+        inherit_device_group = self.__dict__.get('inheritedDevicegroup', False)
+        if inherit_device_group == 'true':
+            self.__dict__.pop('deviceGroup')
+        return self._update(**kwargs)
+
+    def _load(self, **kwargs):
+        '''Load python Service object with response JSON from BigIP.
+
+        :params kwargs: keyword arguments for talking to the device
+        :returns: populated Service object
+        :raises: MissingRequiredReadParameter
+        '''
+
+        key_set = set(kwargs.keys())
+        required_minus_received =\
+            self._meta_data['required_refresh_parameters'] - key_set
+        if required_minus_received != set():
+            error_message = 'Missing required params: %r'\
+                % required_minus_received
+            raise MissingRequiredReadParameter(error_message)
+        name = kwargs.pop('name', '')
+        partition = kwargs.pop('partition', '')
+        read_session = self._meta_data['bigip']._meta_data['icr_session']
+        base_uri = self._meta_data['container']._meta_data['uri']
+
+        name = name.replace('/', '~')
+        load_uri = '%s~%s~%s.app~%s' % (base_uri, partition, name, name)
+
+        response = read_session.get(load_uri, uri_as_parts=False, **kwargs)
+        self._local_update(response.json())
+        self._build_meta_data_uri(self.selfLink)
+        return self
+
+
+class TemplateCollection(Collection):
+    def __init__(self, application):
+        super(TemplateCollection, self).__init__(application)
+        self._meta_data['allowed_lazy_attributes'] = [Template]
+        self._meta_data['collection_registry'] =\
+            {'tm:sys:application:template:templatestate': Template}
+
+
+class Template(Resource):
+    def __init__(self, template_collection):
+        super(Template, self).__init__(template_collection)
+        self._meta_data['required_json_kind'] =\
+            'tm:sys:application:template:templatestate'
---
+++ test/functional/sys/test_sys_application.py
@@ -0,0 +1,170 @@
+# Copyright 2015-2016 F5 Networks Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from requests import HTTPError
+
+TESTDESCRIPTION = 'TESTDESCRIPTION'
+
+# Application Service Dictionary
+sections = {
+    'implementation': '',
+    'presentation': ''
+}
+
+definition = {'definition': sections}
+
+
+def setup_application_test(request, bigip):
+    return bigip.sys.application
+
+
+def setup_template_collection_test(request, bigip):
+    return bigip.sys.application.templatecollection
+
+
+def setup_service_collection_test(request, bigip):
+    return bigip.sys.application.servicecollection
+
+
+def delete_resource(resource):
+    try:
+        resource.delete()
+    except HTTPError as ex:
+        if ex.response.status_code != 404:
+            raise
+
+
+def setup_template_test(request, bigip, name, partition):
+    def teardown():
+        delete_resource(test_template)
+
+    request.addfinalizer(teardown)
+    template_collection = setup_template_collection_test(request, bigip)
+    test_template = template_collection.template
+    return template_collection, test_template.create(
+        name=name,
+        partition=partition,
+        actions=definition
+    )
+
+
+def setup_service_test(request, bigip, name, partition, template_name):
+    def teardown():
+        delete_resource(test_service)
+        delete_resource(test_template)
+
+    request.addfinalizer(teardown)
+    service_collection = setup_service_collection_test(request, bigip)
+    test_template = setup_template_collection_test(request, bigip).template
+
+    test_template.create(
+        name=template_name,
+        partition=partition,
+        actions=definition
+    )
+    test_service = None
+    test_service = service_collection.service.create(
+        name=name, partition=partition, template=template_name
+    )
+    return service_collection, test_service
+
+
+def curdl_check(collection, resource, resource_name, **kwargs):
+    name = kwargs['name']
+    assert resource.name == name
+    second_resource = getattr(collection, resource_name).load(**kwargs)
+
+    assert second_resource.name == resource.name
+    assert second_resource.generation == resource.generation
+
+    resource.description = TESTDESCRIPTION
+    resource.update()
+    assert resource.description == TESTDESCRIPTION
+    assert resource.generation > second_resource.generation
+
+    second_resource.refresh()
+    assert second_resource.generation == resource.generation
+
+
+class TestApplication(object):
+    def test_get_collection(self, request, bigip):
+        app_org_collection = setup_application_test(request, bigip)
+        app_org_full_collection = app_org_collection.get_collection()
+        assert len(app_org_full_collection) == 4
+
+
+class TestTemplateCollection(object):
+    def test_get_collection(self, request, bigip):
+        templ_collection = setup_template_collection_test(request, bigip)
+        all_templates = templ_collection.get_collection()
+        assert len(all_templates) == 27
+        for templ in all_templates:
+            assert templ.verificationStatus == 'signature-verified'
+            assert templ.totalSigningStatus == 'one-cert-signed'
+
+
+class TestTemplate(object):
+    def test_template_CURDL(self, request, bigip):
+        templ_collection, test_templ = setup_template_test(
+            request,
+            bigip,
+            'test_template',
+            'Common'
+        )
+        curdl_check(
+            templ_collection,
+            test_templ,
+            'template',
+            name='test_template'
+        )
+
+
+class TestServiceCollection(object):
+    def test_get_collection(self, request, bigip):
+        serv_collection, test_service = setup_service_test(
+            request,
+            bigip,
+            'test_service',
+            'Common',
+            'test_template'
+        )
+
+        all_services = serv_collection.get_collection()
+        assert len(all_services) is 1
+        # Description field does not exist until assigned
+        # The creation of this field is tested in check_curdl
+        assert hasattr(all_services[0], 'description') is False
+        assert all_services[0].name == 'test_service'
+
+
+class TestService(object):
+    def test_service_CURDL(self, request, bigip):
+        serv_collection, test_serv = setup_service_test(
+            request,
+            bigip,
+            'test_service',
+            'Common',
+            'test_template'
+        )
+        # Make sure the uri is what we expect
+        assert bigip._meta_data['uri'] + 'sys/application/service/~Common' \
+            '~test_service.app~test_service' in test_serv._meta_data['uri']
+        curdl_check(
+            serv_collection,
+            test_serv,
+            'service',
+            name='test_service',
+            partition='Common'
+        )
