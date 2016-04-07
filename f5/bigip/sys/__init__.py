d5c60b970feb061296e3a4e41330be61e576d5c5
wypior <w.wypior@f5.com>
2016-04-07 10:01:13 +0100

    Issues: Fixes #339
    Problem:
    Add device DNS configuration support to SDK
    
    Analysis:
    * Added DNS unnamed resource class, changed __init__ in sys to reflect that, added functional test into Test suite
    
    Tests:
    * Functional Test

--- f5/bigip/sys/__init__.py
+++ f5/bigip/sys/__init__.py
@@ -34,7 +34,7 @@ from f5.bigip.sys.failover import Failover
 from f5.bigip.sys.folder import Folders
 from f5.bigip.sys.global_settings import Global_Settings
 from f5.bigip.sys.ntp import Ntp
-from f5.bigip.sys.ntp import Dns
+from f5.bigip.sys.dns import Dns
 from f5.bigip.sys.performance import Performance
 
 
