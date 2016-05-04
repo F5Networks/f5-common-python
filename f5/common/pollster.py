f15b3fb181b35b451387f1a59f83c57285c97bab
Paul Breaux <p.breaux@f5.com>
2016-05-04 16:10:41 -0600

    Adding the cluster_manager module
    Issues:
    Fixes #400
    
    Problem:
    Initial commit for cluster_manager module for managing a BigIP cluster
    
    Analysis:
    Initial commit is using one large module to do all of the heavy lifting.
    Will break out in future commits when common functionality arises
    
    Tests:
    Only manual tests so far

---
+++ f5/common/pollster.py
@@ -0,0 +1,43 @@
+# Copyright 2016 F5 Networks Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+import functools
+import time
+
+
+class MaximumAttemptsReached(Exception):
+    pass
+
+
+def poll_by_method(method, attempts=30, interval=2):
+    '''Poll with a given method for a specified number of times.
+
+    :param method: callable to invoke in loop -- if no exception is raised
+                    the call is considered succeeded
+    :param attempts: number of iterations to attempt
+    :param interval: seconds to wait before next attempt
+    '''
+
+    @functools.wraps(method)
+    def poll(*args, **kwargs):
+        for attempt in range(attempts):
+            try:
+                return method(*args, **kwargs)
+            except Exception:
+                time.sleep(interval)
+                continue
+        raise MaximumAttemptsReached('Polled method maximum number of times')
+    return poll
