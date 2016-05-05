f415660bbe2835601b13dafae32c501172a9d254
Tim Rupp <caphrim007@gmail.com>
2016-05-05 10:38:59 -0700

    Adds the auth/password-policy api to the skd
    Issues:
    Fixes #374
    
    Problem:
    The issue is that the password_policy endpoint is not available in
    the sdk
    
    Analysis:
    This change adds the password-policy api to the sdk. This change was
    needed to allow access to these settings so that they can be updated
    
    Tests:
      * f5/bigip/auth/test/test_password_policy.py
      * test/functional/auth/test_password_policy.py

---
+++ f5/bigip/auth/test/test_password_policy.py
@@ -0,0 +1,40 @@
+# Copyright 2016 F5 Networks Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import mock
+import pytest
+
+from f5.bigip.auth.password_policy import Password_Policy
+from f5.bigip.mixins import UnsupportedMethod
+
+
+@pytest.fixture
+def FakePasswordPolicy():
+    fake_sys = mock.MagicMock()
+    return Password_Policy(fake_sys)
+
+
+def test_create_raises(FakePasswordPolicy):
+    with pytest.raises(UnsupportedMethod) as EIO:
+        FakePasswordPolicy.create()
+    assert EIO.value.message == \
+        "Password_Policy does not support the create method"
+
+
+def test_delete_raises(FakePasswordPolicy):
+    with pytest.raises(UnsupportedMethod) as EIO:
+        FakePasswordPolicy.delete()
+    assert EIO.value.message == \
+        "Password_Policy does not support the delete method"
