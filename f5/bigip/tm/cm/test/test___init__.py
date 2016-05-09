779d28060e54d31737461fd37a8c0b51566e01ac
Paul Breaux <p.breaux@f5.com>
2016-05-09 17:19:40 -0600

    Sync to group in cm failing to sync
    Issues:
    Fixes #396
    
    Problem:
    This is failing silently to sync the BigIP to group.
    
    Analysis:
    I have implemented a working example of syncing to and from group, yet
    the caveat is that there is no good way to determine whether a group (or
    device) has been synced. This is as far as I know. I perused each uri
    path for the device_group and for the device itself, and came up with
    nothing. Nevertheless, the tests pass.
    
    Tests: All tests pass

---
+++ f5/bigip/tm/cm/test/test___init__.py
@@ -0,0 +1,48 @@
+# Copyright 2016 F5 Networks Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from f5.bigip import BigIP
+
+import mock
+import pytest
+
+
+@pytest.fixture
+def FakeiControl():
+    bigip = BigIP('host', 'fake_admin', 'fake_admin')
+    bigip._meta_data['icr_session'] = mock.MagicMock()
+    return bigip
+
+
+class TestCMSync(object):
+    def test_sync_to_group(self, FakeiControl):
+        cm = FakeiControl.cm
+        cm.sync_to_group('test')
+        session = cm._meta_data['bigip']._meta_data['icr_session']
+        session.post.assert_called_with(
+            'https://host:443/mgmt/tm/cm/',
+            json={'utilCmdArgs': 'config-sync to-group test', 'command': 'run'}
+        )
+
+    def test_sync_from_group(self, FakeiControl):
+        cm = FakeiControl.cm
+        cm.sync_from_group('test')
+        session = cm._meta_data['bigip']._meta_data['icr_session']
+        session.post.assert_called_with(
+            'https://host:443/mgmt/tm/cm/',
+            json={
+                'utilCmdArgs': 'config-sync from-group test', 'command': 'run'
+            }
+        )
