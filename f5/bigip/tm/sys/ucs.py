425309cffeba2de4d4fb4e4bd9c509a05d3bf10a
wypior <w.wypior@f5.com>
2016-06-27 15:39:42 +0100

    adds UCS support

--- f5/bigip/tm/sys/ucs.py
+++ f5/bigip/tm/sys/ucs.py
@@ -26,17 +26,18 @@ REST Kind
     ``tm:sys:ucs:*``
 """
 
-from f5.bigip.mixins import UnnamedResourceMixin
 from f5.bigip.mixins import CommandExecutionMixin
+from f5.bigip.mixins import UnnamedResourceMixin
+from f5.bigip.mixins import InvalidCommand
 from f5.bigip.resource import Collection
 from f5.bigip.resource import ResourceBase
+from requests.exceptions import HTTPError
 
 
 class Ucs_s(Collection, CommandExecutionMixin):
     """BIG-IP® system UCS collection
 
         .. note::
-
 
     """
     def __init__(self, sys):
@@ -45,6 +46,35 @@ class Ucs_s(Collection, CommandExecutionMixin):
         self._meta_data['allowed_commands'].extend(['load', 'save'])
         self._meta_data['attribute_registry'] = \
             {'tm:sys:ucs:ucsstate': Ucs}
+        self._meta_data['minimum_version'] = '12.0.0'
+
+    def exec_cmd(self, command, **kwargs):
+
+        cmds = self._meta_data['allowed_commands']
+
+        if command not in self._meta_data['allowed_commands']:
+            error_message = "The command value {0} does not exist" \
+                            "Valid commands are {1}".format(command, cmds)
+            raise InvalidCommand(error_message)
+
+        if command == 'load':
+            kwargs['command'] = command
+            self._check_exclusive_parameters(**kwargs)
+            requests_params = self._handle_requests_params(kwargs)
+            self._check_command_parameters(**kwargs)
+            session = self._meta_data['bigip']._meta_data['icr_session']
+            try:
+
+                session.post(
+                    self._meta_data['uri'], json=kwargs, **requests_params)
+
+            except HTTPError as err:
+                if err.response.status_code != 502:
+                    raise
+                return
+
+        else:
+            return self._exec_cmd(command, **kwargs)
 
 
 class Ucs(UnnamedResourceMixin, ResourceBase):
