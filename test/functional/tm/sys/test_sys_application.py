26ec45daba77ce7b03b540e8c50d9d3ab4631ae5
Paul Breaux <p.breaux@f5.com>
2016-08-02 15:51:30 -0600

    Updating with comments from zancas.

--- test/functional/tm/sys/test_sys_application.py
+++ test/functional/tm/sys/test_sys_application.py
@@ -13,16 +13,14 @@
 # limitations under the License.
 #
 
-from pprint import pprint as pp
 from requests import HTTPError
 
 from f5.bigip.tm.sys.application import Service
 from f5.bigip.tm.sys.application import Template
 
-pp(__file__)
 TESTDESCRIPTION = 'TESTDESCRIPTION'
 
-# Application Service Dictionary
+# Application service dictionary, which matches JSON structure
 sections = {
     'implementation': '',
     'presentation': ''
@@ -56,6 +54,7 @@ def setup_template_test(request, bigip, name, partition):
 
     def teardown():
         delete_resource(test_template)
+
     request.addfinalizer(teardown)
     test_template = template_s.template.create(
         name=name,
@@ -63,6 +62,7 @@ def setup_template_test(request, bigip, name, partition):
         actions=definition
     )
     assert isinstance(test_template, Template)
+    assert id(test_template) != id(template_s.template)
     return template_s, test_template
 
 
@@ -70,6 +70,7 @@ def setup_service_test(request, bigip, name, partition, template_name, tgroup):
     service_s = setup_service_collection_test(request, bigip)
 
     def teardown():
+        # Delete the service first, then the template
         delete_resource(test_service)
         delete_resource(test_template)
 
@@ -80,6 +81,7 @@ def setup_service_test(request, bigip, name, partition, template_name, tgroup):
         actions=definition
     )
     assert isinstance(test_template, Template)
+    assert id(test_template) != id(template_factory)
     test_service = service_s.service.create(
         name=name,
         partition=partition,
@@ -87,6 +89,7 @@ def setup_service_test(request, bigip, name, partition, template_name, tgroup):
         trafficGroup=tgroup
     )
     assert isinstance(test_service, Service)
+    assert id(test_service) != id(service_s.service)
     request.addfinalizer(teardown)
     return service_s, test_service
 
