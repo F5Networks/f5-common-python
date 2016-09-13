e2ad5ec8f43cc9753644254fe37332cd2174a577
rahm <j.rahm@f5.com>
2016-09-13 17:45:37 -0500

    Issue #538 - Adding bash utility

---
+++ test/functional/tm/util/test_bash.py
@@ -0,0 +1,59 @@
+
+# Copyright 2016 F5 Networks Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import pytest
+
+from f5.utils.util_exceptions import UtilError
+from icontrol.session import iControlUnexpectedHTTPError
+
+
+def test_E_bash(mgmt_root):
+
+    # use bash to create a test file
+    bash1 = mgmt_root.tm.util.bash.exec_cmd(
+        'run',
+        utilCmdArgs='-c "echo hello >> /var/tmp/test.txt"')
+
+    # commandResult should not be present if this was successful
+    assert 'commandResult' not in bash1.__dict__
+
+    # UtilError should be raised with bad bash command option
+    with pytest.raises(UtilError) as err:
+        mgmt_root.tm.util.unix_ls.exec_cmd('run',
+                                           utilCmdArgs='-9 hello')
+        assert 'invalid option' in err.response.text
+
+    # UtilError should be raised if command isn't found
+    with pytest.raises(UtilError) as err:
+        mgmt_root.tm.util.unix_ls.exec_cmd('run',
+                                           utilCmdArgs='-c hello')
+        assert 'command not found' in err.response.text
+
+    # UtilError should be raised if no command option and a string
+    with pytest.raises(UtilError) as err:
+        mgmt_root.tm.util.unix_ls.exec_cmd('run',
+                                           utilCmdArgs='hello')
+        assert 'No such file or directory' in err.response.text
+
+    # clean up test file
+    mgmt_root.tm.util.unix_rm.exec_cmd('run', utilCmdArgs='/var/tmp/test.txt')
+
+    # test that unmatched quotes errors out
+    with pytest.raises(iControlUnexpectedHTTPError) as err:
+        mgmt_root.tm.util.unix_ls.exec_cmd('run',
+                                           utilCmdArgs='-c "df -k')
+        assert err.response.status_code == 400
+        assert 'quotes are not balanced' in err.response.text
