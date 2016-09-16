a5ce730a3d1944928ffb808d64e23c4ac92de11d
rahm <j.rahm@f5.com>
2016-09-15 21:57:07 -0500

    Issue #538 - Adding the dig DNS utility
    Updated Files for Dig
    f5/bigip/tm/util/__init__.py
    
    Added Files for Dig
    f5/bigip/tm/util/Dig.py
    f5/bigip/tm/util/test/test_dig.py
    test/functional/tm/util/test_dig.py
    
    Updated Files for Bash (typo fixes from previous PR)
    f5/bigip/tm/util/Bash.py
    test/functional/tm/util/test_bash.py

--- f5/bigip/tm/util/Dig.py
+++ f5/bigip/tm/util/Dig.py
@@ -17,13 +17,13 @@
 """BIG-IP® utility module
 
 REST URI
-    ``http://localhost/mgmt/tm/util/bash``
+    ``http://localhost/mgmt/tm/util/dig``
 
 GUI Path
     N/A
 
 REST Kind
-    ``tm:util:bash:*``
+    ``tm:util:dig:*``
 """
 
 from f5.bigip.mixins import CommandExecutionMixin
@@ -31,7 +31,7 @@ from f5.bigip.resource import UnnamedResource
 from f5.utils.util_exceptions import UtilError
 
 
-class Bash(UnnamedResource, CommandExecutionMixin):
+class Dig(UnnamedResource, CommandExecutionMixin):
     """BIG-IP® utility command
 
     .. note::
@@ -41,9 +41,9 @@ class Bash(UnnamedResource, CommandExecutionMixin):
     """
 
     def __init__(self, util):
-        super(Bash, self).__init__(util)
+        super(Dig, self).__init__(util)
         self._meta_data['required_command_parameters'].update(('utilCmdArgs',))
-        self._meta_data['required_json_kind'] = 'tm:util:bash:runstate'
+        self._meta_data['required_json_kind'] = 'tm:util:dig:runstate'
         self._meta_data['allowed_commands'].append('run')
 
     def _exec_cmd(self, command, **kwargs):
@@ -52,18 +52,14 @@ class Bash(UnnamedResource, CommandExecutionMixin):
         requests_params = self._handle_requests_params(kwargs)
         self._check_command_parameters(**kwargs)
 
-        if not kwargs['utilCmdArgs'].startswith("-c"):
-            raise UtilError(
-                'Required format is "-c <bash command and arguments>"')
-
         session = self._meta_data['bigip']._meta_data['icr_session']
         response = session.post(
             self._meta_data['uri'], json=kwargs, **requests_params)
         self._local_update(response.json())
 
         if 'commandResult' in self.__dict__:
-            if self.commandResult.startswith('/bin/bash'):
-                raise UtilError('%s' % self.commandResult.split(' ', 1)[1])
+            if 'Invalid option' in self.commandResult:
+                raise UtilError('%s' % self.commandResult)
             else:
                 return self
         else:
---
+++ test/functional/tm/util/test_dig.py
@@ -0,0 +1,45 @@
+
+# Copyright 2016 F5 Networks Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import pytest
+
+from f5.bigip.resource import MissingRequiredCommandParameter
+from f5.utils.util_exceptions import UtilError
+from icontrol.session import iControlUnexpectedHTTPError
+
+
+def test_E_dig(mgmt_root):
+
+    with pytest.raises(MissingRequiredCommandParameter) as err:
+        mgmt_root.tm.util.dig.exec_cmd('run')
+        assert "Missing required params: ['utilCmdArgs']" in err.response.text
+
+    dig1 = mgmt_root.tm.util.dig.exec_cmd('run', utilCmdArgs='f5.com NS')
+
+    # commandResult should be present with data from 'f5.com NS'
+    assert 'commandResult' in dig1.__dict__
+    assert 'DiG' in dig1.commandResult
+
+    # UtilError should be raised if there is an invalid option for dig
+    with pytest.raises(UtilError) as err:
+        mgmt_root.tm.util.dig.exec_cmd('run', utilCmdArgs='-9 f5.com NS')
+        assert 'Invalid option' in err.response.text
+
+    # iControlUnexpectedHTTPError should be raised if quotes don't match
+    with pytest.raises(iControlUnexpectedHTTPError) as err:
+        mgmt_root.tm.util.dig.exec_cmd('run', utilCmdArgs='"')
+        assert err.response.status_code == 400
+        assert 'quotes are not balanced' in err.response.text
