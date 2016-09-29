333af3e1bc57b2d5e5fd9eb613b3f57f89770383
wypior <w.wypior@f5.com>
2016-09-29 16:54:18 +0100

    Addresses SNMP  issues in 12.1
    Fixes Issues: #735
    
    Updated Files:
    
        test/functional/net/test_snmp.py
        f5/bigip/tm/sys/test/test_snmp.py
        f5/bigip/tm/sys/snmp.py
    
    Updates:
    
    In 12.1.0 there are problems with decryption of the hashed password when using update() method. Given the number of ways this can break, the update() method has been disabled for Users and Traps. The error message returns advisory to use modify() method which functions correctly.
    
    Tests:
    
    Flake8
    Functional Test
    Unit Tests

--- f5/bigip/tm/sys/test/test_snmp.py
+++ f5/bigip/tm/sys/test/test_snmp.py
@@ -17,13 +17,32 @@ import mock
 import pytest
 
 from f5.bigip.mixins import UnsupportedMethod
+from f5.bigip.resource import UnsupportedOperation
 from f5.bigip.tm.sys.snmp import Snmp
+from f5.bigip.tm.sys.snmp import Trap
+from f5.bigip.tm.sys.snmp import User
 
 
 @pytest.fixture
 def FakeSnmp():
     fake_sys = mock.MagicMock()
     return Snmp(fake_sys)
+
+
+@pytest.fixture
+def FakeSnmpUser():
+    fake_sys = mock.MagicMock()
+    fake_user = User(fake_sys)
+    fake_user._meta_data['bigip'].tmos_version = '12.1.0'
+    return fake_user
+
+
+@pytest.fixture
+def FakeTrapUser():
+    fake_sys = mock.MagicMock()
+    fake_trap = Trap(fake_sys)
+    fake_trap._meta_data['bigip'].tmos_version = '12.1.0'
+    return fake_trap
 
 
 def test_create_raises(FakeSnmp):
@@ -36,3 +55,19 @@ def test_delete_raises(FakeSnmp):
     with pytest.raises(UnsupportedMethod) as EIO:
         FakeSnmp.delete()
     assert EIO.value.message == "Snmp does not support the delete method"
+
+
+def test_update_user_raises(FakeSnmpUser):
+    with pytest.raises(UnsupportedOperation) as EIO:
+        FakeSnmpUser.update()
+    assert EIO.value.message == "Update() is unsupported for User on " \
+                                "version 12.1.0. " \
+                                "Utilize Modify() method instead"
+
+
+def test_update_trap_raises(FakeTrapUser):
+    with pytest.raises(UnsupportedOperation) as EIO:
+        FakeTrapUser.update()
+    assert EIO.value.message == "Update() is unsupported for Trap on " \
+                                "version 12.1.0. " \
+                                "Utilize Modify() method instead"
