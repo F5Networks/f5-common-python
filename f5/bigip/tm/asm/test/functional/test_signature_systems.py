26a345023b1d7d7cb75a2fbfcfbba30f6ecdfb74
Wojciech Wypior <w.wypior@f5.com>
2016-10-28 01:04:08 +0100

    Adding ASM Signature Systems (#791)
    * Adding ASM Signature Systems
    
    Issues:
    Fixes #706
    
    Problem:
    Signature Systems were missing from SDK, as they are crucial when configuring ASM policy they were a blocker for phase 2
    
    Analysis:
    Added Signature Systems endpoint to SDK with relevant tests, also correct minor typos in signature update class.
    
    Files Changed/Added:
    
    f5/bigip/asm/__init__.py
    f5/bigip/asm/signature_systems.py
    f5/bigip/asm/signature_update.py
    f5/bigip/asm/test/unit/test_signature_systems.py
    f5/bigip/asm/test/functional/test_signature_systems.py
    
    Tests:
    Flake 8
    Unit
    Functional
    
    * Adding ASM Signature Systems
    
    Issues:
    Fixes #706
    
    Problem:
    Signature Systems were missing from SDK, as they are crucial when configuring ASM policy they were a blocker for phase 2
    
    Analysis:
    Added Signature Systems endpoint to SDK with relevant tests, also correct minor typos in signature update class.
    
    Files Changed/Added:
    
    f5/bigip/asm/__init__.py
    f5/bigip/asm/signature_systems.py
    f5/bigip/asm/signature_update.py
    f5/bigip/asm/test/unit/test_signature_systems.py
    f5/bigip/asm/test/functional/test_signature_systems.py
    
    Tests:
    Flake 8
    Unit
    Functional

---
+++ f5/bigip/tm/asm/test/functional/test_signature_systems.py
@@ -0,0 +1,62 @@
+# Copyright 2016 F5 Networks Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+from f5.bigip.tm.asm.signature_systems import Signature_System
+from pprint import pprint as pp
+
+
+class TestSignatureSystems(object):
+    def test_collection(self, request, mgmt_root):
+        coll = mgmt_root.tm.asm.signature_systems_s.get_collection()
+        assert coll
+        assert coll[0] is not coll[1]
+        assert isinstance(coll[0], Signature_System)
+        assert isinstance(coll[1], Signature_System)
+        assert coll[0].kind == coll[1].kind
+        assert coll[0].id != coll[1].id
+        assert coll[0].name != coll[1].name
+
+    def test_load(self, request, mgmt_root):
+        coll = mgmt_root.tm.asm.signature_systems_s.get_collection()
+        idhash1 = str(coll[0].id)
+        sig1 = mgmt_root.tm.asm.signature_systems_s.signature_system.load(
+            id=idhash1)
+        idhash2 = str(coll[1].id)
+        sig2 = mgmt_root.tm.asm.signature_systems_s.signature_system.load(
+            id=idhash2)
+        assert sig1.kind == 'tm:asm:signature-systems:signature-systemstate'
+        assert sig2.kind == sig1.kind
+        assert sig1.name != sig2.name
+        assert sig1.id == coll[0].id
+        assert sig2.id == coll[1].id
+        pp(sig1.raw)
+        pp(sig2.raw)
+
+    def test_refresh(self, request, mgmt_root):
+        coll = mgmt_root.tm.asm.signature_systems_s.get_collection()
+        idhash1 = str(coll[0].id)
+        sig1 = mgmt_root.tm.asm.signature_systems_s.signature_system.load(
+            id=idhash1)
+        sig2 = mgmt_root.tm.asm.signature_systems_s.signature_system.load(
+            id=idhash1)
+
+        assert sig1.kind == 'tm:asm:signature-systems:signature-systemstate'
+        sig2.refresh()
+        assert sig2.kind == sig1.kind
+        assert sig1.name == sig2.name
+        assert sig1.id == coll[0].id
+        assert sig2.id == coll[0].id
+        pp(sig1.raw)
+        pp(sig2.raw)
