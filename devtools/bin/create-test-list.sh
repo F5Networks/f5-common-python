fc81dbc1d984f4cd2a4b39ff414b4af3c5aef246
Tim Rupp <caphrim007@gmail.com>
2016-12-30 13:18:15 -0800

    Corrects the file test detection
    Issues:
    
    Problem:
    The original script to check for files to test was too simple
    
    Analysis:
    This change adds better logic to detect what needs to be tested
    
    Tests:

--- devtools/bin/create-test-list.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git --no-pager diff --name-only origin/development $GIT_COMMIT | egrep -v '(^requirements\.|^setup.py)' | egrep '(^f5\/iworkflow\/)' > pytest.iworkflow.jenkins.txt
-git --no-pager diff --name-only origin/development $GIT_COMMIT | egrep -v '(^requirements\.|^setup.py)' | egrep '(^f5\/bigip\/)' > pytest.bigip.jenkins.txt
-git --no-pager diff --name-only origin/development $GIT_COMMIT | egrep -v '(^requirements\.|^setup.py)' | egrep '(^f5\/bigiq\/)' > pytest.bigiq.jenkins.txt
