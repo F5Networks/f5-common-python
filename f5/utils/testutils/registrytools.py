220829ef6fcbae67c5aef036d16c7f5876470f86
Za Wilgustus <za@f5.com>
2017-01-12 16:43:44 -0700

    utilities that belong here were elsewhere
    Issues:
    Fixes #911
    
    Problem:  Misplaced logic made dependency mamagement
    unnecessarily complex.
    
    Analysis: If there're facilities that are strictly used to
    augment tests that depend on the SDK, then they belong in
    the SDK.

--- f5/utils/testutils/registrytools.py
+++ f5/utils/testutils/registrytools.py
@@ -22,6 +22,48 @@ from f5.bigip.resource import OrganizingCollection
 from f5.bigip.tm.asm import Asm
 
 
+
+
+AGENT_LB_DEL_ORDER = {'/mgmt/tm/ltm/virtual':           1,
+                      '/mgmt/tm/ltm/pool':              2,
+                      'mgmt/tm/ltm/node/':              3,
+                      'monitor':                        4,
+                      'virtual-address':                5,
+                      'mgmt/tm/net/self/':              6,
+                      '/mgmt/tm/net/fdb':               7,
+                      'mgmt/tm/net/tunnels/tunnel/':    8,
+                      'mgmt/tm/net/tunnels/vxlan/':     9,
+                      'mgmt/tm/net/tunnels/gre':       10,
+                      'mgmt/tm/net/vlan':              11,
+                      'route':                         12,
+                      '/mgmt/tm/ltm/snatpool':         13,
+                      '/mgmt/tm/ltm/snat-translation': 14,
+                      '/mgmt/tm/net/route-domain':     15,
+                      '/mgmt/tm/sys/folder':           16}
+
+
+def order_by_weights(unordered, weights_table):
+    '''(iterable, wieghts) --> iterable_ordered_by_weights
+
+    AGENT_LB_DEL_ORDER is an example weights table.  Pass it as the second
+    argument where the first is a list of BigIP device URIs and it will return
+    a list with the resources in the appropriate order for deletion.
+    Note that URIS that do not match a key in the weights table are set to have
+    the same "high" weight.   Their relative order will not change.
+
+    >>> order_by_weights([URI1, URI2, ...], AGENT_LB_DEL_ORDER)
+    [URI2, URI1, ....]
+    '''
+    min_minus_one = min(weights_table.values()) - 1
+
+    def order_key(item):
+        for k in weights_table:
+            if k in item:
+                return weights_table[k]
+        return min_minus_one
+    ordered_by_weights = sorted(list(unordered), key=order_key)
+    return ordered_by_weights
+
 def register_collection_atoms(collection):
     '''Given a collection return a registry of all of its atoms (elements).
 
