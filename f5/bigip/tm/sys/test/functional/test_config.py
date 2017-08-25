28c1ea0329b0f9feefb0a63cacc19bc6b7be5fbb
rahm <j.rahm@f5.com>
2017-08-25 14:52:19 -0500

    Issue #1254: Clean up config functional tests, enhance merge syntax
    Files Changed
     - tm/sys/config.py
     - tm/sys/test/functional/test_config.py

--- f5/bigip/tm/sys/test/functional/test_config.py
+++ f5/bigip/tm/sys/test/functional/test_config.py
@@ -14,16 +14,43 @@
 #
 
 from f5.sdk_exception import UnsupportedMethod
+from tempfile import NamedTemporaryFile
 
+import os
 import pytest
 
 
 class TestConfig(object):
-    def test_save(self, bigip):
-        c = bigip.sys.config
-        c.exec_cmd('save')
+    def test_save(self, mgmt_root):
+        c = mgmt_root.tm.sys.config
+        config_save = c.exec_cmd('save')
+        assert config_save.command == 'save'
 
-    def test_update(selfself, mgmt_root):
+    def test_load(self, mgmt_root):
+        c = mgmt_root.tm.sys.config
+        config_load = c.exec_cmd('load')
+        assert config_load.command == 'load'
+
+    def test_merge(self, mgmt_root):
+        # Create a BIG-IP object to merge
+        ntf = NamedTemporaryFile(delete=False)
+        ntf_basename = os.path.basename(ntf.name)
+        ntf.write('ltm pool mergepool { }')
+        ntf.seek(0)
+        # upload the file to BIG-IP
+        mgmt_root.shared.file_transfer.uploads.upload_file(ntf.name)
+        c = mgmt_root.tm.sys.config
+        config_merge = c.exec_cmd('load',
+                                  merge=True,
+                                  file='/var/config/rest/downloads/{0}'.format(ntf_basename))
+        assert config_merge.command == 'load'
+        assert config_merge.options[0]['merge'] is True
+        assert mgmt_root.tm.ltm.pools.pool.exists(name='mergepool') is True
+
+        p1 = mgmt_root.tm.ltm.pools.pool.load(name='mergepool')
+        p1.delete()
+
+    def test_update(self, mgmt_root):
         with pytest.raises(UnsupportedMethod) as ex:
             mgmt_root.tm.sys.config.update(foo="bar")
         assert 'does not support the update method' in ex.value.message
