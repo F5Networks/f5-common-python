3dbbbd0eaeafee49dc6415deee5fe67a110c74d2
Prashant sunkari <sprashant87@gmail.com>
2017-11-03 14:43:30 -0700

    Issues: Fixes #1317
    Problem: Add support for lsnpool(CGNAT)
    
    Analysis:
    Added two things:
    - LSN log profile collection and resource + corresponding tests
    - Add missing ALG profile - IPsecALG
    
    Tests:
    - Added functional tests for lsnpool and lsn log profile
    - Test results against BIGIP with 14.0.0 build
    cmd: py.test test_lsnpool.py --ignore=f5/ --bigip=172.27.77.41 -vs
    
    test_lsnpool.py::TestLSNPool::test_create_no_args PASSED
    test_lsnpool.py::TestLSNPool::test_create PASSED
    test_lsnpool.py::TestLSNPool::test_create_with_logProfile_without_logPub PASSED
    test_lsnpool.py::TestLSNPool::test_create_with_logProfile PASSED
    test_lsnpool.py::TestLSNPool::test_refresh PASSED
    test_lsnpool.py::TestLSNPool::test_update PASSED
    test_lsnpool.py::TestLSNPool::test_exists PASSED
    test_lsnpool.py::TestLSNPool::test_stats PASSED
    test_lsnpool.py::TestLSNPool::test_create_with_nondefault_mode PASSED
    test_lsnpool.py::TestLSNPool::test_create_with_invalid_mode PASSED
    test_lsnpool.py::TestLSNPool::test_create_with_member PASSED
    test_lsnpool.py::TestLSNPool::test_virtual_with_pool PASSED
    test_lsnpool.py::TestLSNPool::test_collection PASSED
    test_lsnpool.py::TestLSNPoolLogProfile::test_create_no_args PASSED
    test_lsnpool.py::TestLSNPoolLogProfile::test_create PASSED
    test_lsnpool.py::TestLSNPoolLogProfile::test_update PASSED
    test_lsnpool.py::TestLSNPoolLogProfile::test_exists PASSED

--- f5/bigip/tm/ltm/__init__.py
+++ f5/bigip/tm/ltm/__init__.py
@@ -65,7 +65,6 @@ class Ltm(OrganizingCollection):
             LSNPools,
             Nats,
             Nodes,
-            LSNPools,
             Persistence,
             Policys,
             Pools,
