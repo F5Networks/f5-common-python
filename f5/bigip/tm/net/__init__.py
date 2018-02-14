42accb72cd90d6a662c9496d65a2363937a73bff
Tim Rupp <caphrim007@gmail.com>
2018-02-13 20:42:57 -0800

    Adds timer-policy API
    Issues:
    Fixes #1390
    
    Problem:
    Net timer policy api did not exist
    
    Analysis:
    This adds it
    
    Tests:
    functional

--- f5/bigip/tm/net/__init__.py
+++ f5/bigip/tm/net/__init__.py
@@ -35,6 +35,7 @@ from f5.bigip.tm.net.interface import Interfaces
 from f5.bigip.tm.net.route import Routes
 from f5.bigip.tm.net.route_domain import Route_Domains
 from f5.bigip.tm.net.selfip import Selfips
+from f5.bigip.tm.net.timer_policy import Timer_Policys
 from f5.bigip.tm.net.trunk import Trunks
 from f5.bigip.tm.net.tunnels import TunnelS
 from f5.bigip.tm.net.vlan import Vlans
@@ -51,6 +52,7 @@ class Net(OrganizingCollection):
             Routes,
             Route_Domains,
             Selfips,
+            Timer_Policys,
             Trunks,
             TunnelS,
             Vlans
